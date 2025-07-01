948f91cb1f1914721f68b7a8171baff04ff8c058
krishxkapoor <krishxkapoor@gmail.com>
2025-07-01 23:48:09 +0530

    firstt

---
+++ Operations.js
@@ -0,0 +1,13 @@
+ 
+    function showOptions() {
+      const box = document.getElementById("domainOptions");
+      box.style.display = box.style.display === "none" ? "block" : "none";
+    }
+    function showOptions() {
+      const box = document.getElementById("websiteshosting-options");
+      box.style.display = box.style.display === "none" ? "block" : "none";
+    }
+    function showOptions() {
+      const box = document.getElementById("securityoption");
+      box.style.display = box.style.display === "none" ? "block" : "none";
+    }
